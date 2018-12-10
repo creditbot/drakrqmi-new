340f01c847049f0be44acf6ed09a4073c25ef3c7
creditbot <44919914+creditbot@users.noreply.github.com>
2018-12-10 20:30:54 +0300

    Update bots.js

--- bots.js
+++ bots.js
@@ -1276,7 +1276,7 @@ client.on('guildMemberAdd', member => {
     let embed = new Discord.RichEmbed()
         .setColor('RANDOM')
         .setThumbnail(memberavatar)
-        .addField('مرحبا صديقي' , `مرحبآ بك في سيرفر **${member}**`)
+        .addField('مرحبا صديقي' , `مرحبا بك في سيرفرنا **${member}**`)
         .addField('User Id :', "**[" + `${member.id}` + "]**" )
                 .addField(' عدد الأعضاء',`${member.guild.memberCount}`)
                
@@ -1293,26 +1293,6 @@ return channel.send(embed)
     }
     )});
 
-client.on("message", async message => {
-           let args = message.content.split(' ').slice(1);
-    if(message.content.startsWith(prefix + 'fastrandom')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-    if (message.author.id !== message.guild.owner.id) {     
-    message.channel.send('**هادا الامر لصاحب السيرفر فقط**' );
-      return;
-    }
-    const array = [];
-    message.guild.members.forEach((member) => {
-      array.push(member.user.tag);
-    });
-    const rand = array[Math.floor(Math.random() * array.length)];
-    message.channel.send(rand).then((m) => {
-      m.split('#');
-      m.edit(array);
-    });
-      
-    };
-});
 var KinG66S = {};
 client.on('guildMemberRemove', member => {
 KinG66S[member.id] = {roles: member.roles.array()};
@@ -1633,19 +1613,7 @@ let welcomer = member.guild.channels.find("name","chat");
       }
       });
 	  
-	  client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var iiMo = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setTimestamp()
-        .setTitle('``هي ولد دراك لحق لحق جاتني راسالة جديده في الخاص !``')
-        .setThumbnail(`${message.author.avatarURL}`)
-        .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-        .setFooter(`From **${message.author.tag} (${message.author.id})**`)
-	client.channels.find('name', 'log').send({embed:iiMo});
-    }
-});
+
 
 //test
 
@@ -3575,11 +3543,6 @@ message.channel.sendFile(canvas.toBuffer())
 
 })
 
-
-
-
-
-
 client.on('message', msg => {
 	var prefix = "!";
   if (msg.author.bot) return;
@@ -3629,6 +3592,100 @@ if(message.content.split(' ')[0] == '!bc') {
         });
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("guildDelete", guild => {
+    console.log(` Rqmi Left From Server -- = ${guild.name} = -- , Server Owner -- = ${guild.owner.user.username} = --`)
+   
+var g= guild.members.size
+var i= guild.iconURL
+
+const embed = new Discord.RichEmbed()
+  
+                   .setColor("RANDOM")
+                .setFooter(` ©  Histoire.™ `)
+                .setTitle(" Histoire. Left Server !")
+                .addField("**Guild Name:**", guild.name )
+                 .addField("**Owner:**", guild.owner.user.username )
+                 .addField("**Owner id:**", guild.owner.user.id )
+                 .addField("**MEMBER COUNT:**", g )
+                  .setThumbnail (i)
+client.channels.find('name', 'log').send({ embed: embed });
+});
+
+
+client.on("guildCreate", guild => {
+console.log(` Histoire. Added From Server -- = ${guild.name} = -- , Server Owner -- = ${guild.owner.user.username} = --`)
+
+var g= guild.members.size
+var i= guild.iconURL
+
+const embed = new Discord.RichEmbed()
+  
+                   .setColor("RANDOM")
+                .setFooter(` ©  Histoire.™ `)
+                .setTitle(" Histoire. Added To Server !")
+                .addField("**Guild Name:**", guild.name )
+                 .addField("**Owner:**", guild.owner.user.username )
+                 .addField("**MEMBER COUNT:**", g )
+                  .setThumbnail (i)
+client.channels.find('name', 'log').send({ embed: embed });
+
+});
+
+	  client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var iiMo = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTimestamp()
+        .setTitle('``هي ولد دراك لحق لحق جاتني راسالة جديده في الخاص !``')
+        .setThumbnail(`${message.author.avatarURL}`)
+        .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+        .setFooter(`From **${message.author.tag} (${message.author.id})**`)
+	client.channels.find('name', 'log').send({embed:iiMo});
+    }
+});
+
+
+
+
+
+
+      client.on("message", message => {
+        let args = message.content.split(" ").slice(1);
+      if (message.content.startsWith('!report')) {
+            let user = message.mentions.users.first();
+            let reason = args.slice(1).join(' ');
+            let modlog = client.channels.find('name', 'report');
+            if (!reason) return message.reply('**ضع سبباً مقنعاً**');
+              if (message.mentions.users.size < 1) return message.reply('**يجب عليك منشن للعضو المراد الابلاغ عليه**').catch(console.error);
+       
+        if (!modlog) return message.reply('**لا يوجد روم بأسم report**');
+        const embed = new Discord.RichEmbed()
+          .setColor(0x00AE86)
+          .setTimestamp()
+          .addField('نوع الرسالة:', 'Report')
+          .addField('المراد الابلاغ عليه:', `${user.username}#${user.discriminator} (${user.id}`)
+          .addField('صاحب الابلاغ:', `${message.author.username}#${message.author.discriminator}`)
+          .addField('السبب', reason);
+          message.delete()
+          return client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
+      }
+      });
+
+
 
 
 
